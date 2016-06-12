351616a301289086355d99baaf90abab4a7a0cb9
borovskiu <borovskiu@gmail.com>
2016-06-13 00:04:03 +0300

    hometask lits (7-8)
    tasks

---
+++ task_7_8.java
@@ -0,0 +1,47 @@
+package Homtsk_7_8;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class task_7_8 {
+
+//	1) Встановити MySql Server, налаштувати db connection & db schema. 
+//	Створити таблиці та заповнити їх даними. 
+//	Засобами jdbc підєднатись до створеної таблиці та отримати з неї дані.
+	
+private static final String url = "jdbc:mysql://localhost:3306/lits";
+private static final String user = "root";
+private static final String password = "1";
+
+private static Connection conn;
+private static Statement statem;
+private static ResultSet resume;
+		 
+	public static void main(String args[]) {
+	String query = "select * from students";
+	       try {
+	    	   conn = DriverManager.getConnection(url, user, password);
+		       statem = conn.createStatement();
+		       resume = statem.executeQuery(query);
+		             
+		           while (resume.next()) {
+		                int id = resume.getInt("id");
+		                String name = resume.getString("name");
+		            	int age = resume.getInt("age");
+		                System.out.println(id+ " "+name+" "+age);
+		            }
+		       }
+	       
+	       
+	       catch (SQLException sqlEx) {sqlEx.printStackTrace();}
+	       
+	       finally {
+		            try { conn.close(); } catch(SQLException se) { /*can't do anything */ }
+		            try { statem.close(); } catch(SQLException se) { /*can't do anything */ }
+		            try { resume.close(); } catch(SQLException se) { /*can't do anything */ }
+		        }
+		    }
+
+}
