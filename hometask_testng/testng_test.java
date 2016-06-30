cd04a58aa3d1776c08dae600d942d99340ceb1cd
borovskiu <borovskiu@gmail.com>
2016-06-30 14:55:14 +0300

    LITS_hometask_testng_lesson_3

--- hometask_testng/testng_test.java
+++ hometask_testng/testng_test.java
@@ -1,5 +1,12 @@
 package pack;
 
+//За допомогою http client зробити запит до будь-якого ресурсу і перевірити чи прийшов відповідний статус в response-і,
+//чи тип повернення контенту вірний, розпарсати дані і перевірити чи в даних є дані, які ви очікуєте.
+//Приклад:
+//request http://www.google.com
+//response status: 200 ok
+//response content type: html
+//response body: <html>......<html> (первірити чи буде тут міститись <div id="hplogo"> в response body)
 
 import java.io.IOException;
 import org.apache.http.ParseException;
@@ -9,7 +16,7 @@ import org.apache.http.client.fluent.Request;
 
 public class testng_hometask {
 
-	public static void main(String... args) throws ClientProtocolException,
+	public static void main(String [] args) throws ClientProtocolException,
 			IOException, ParseException {
 		
 		
@@ -20,7 +27,6 @@ public class testng_hometask {
 		    System.out.println(res);
 		
 		
+		
 	}
 }
-
-
