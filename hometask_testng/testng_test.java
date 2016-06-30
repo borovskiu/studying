3fdc1f4a7cc78b3d693543025828f6fe96afde35
borovskiu <borovskiu@gmail.com>
2016-06-30 16:44:18 +0300

    LITS hometask. Lesson 3 TestNG

--- hometask_testng/testng_test.java
+++ hometask_testng/testng_test.java
@@ -9,23 +9,36 @@ package pack;
 //response body: <html>......<html> (первірити чи буде тут міститись <div id="hplogo"> в response body)
 
 import java.io.IOException;
+
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 
 public class testng_hometask {
 
+	public boolean results(boolean result)
+	{
+		return result;
+	} 
+	
+	@Test
 	public static void main(String [] args) throws ClientProtocolException,
 			IOException, ParseException {
 		
-		
+		//Получаю status и content type
 		   System.out.println(Request.Get("http://google.com.ua").execute().returnResponse());
+		//Получаю body. Преобразую в строку и вывожу  
 		   String smth = (Request.Get("http://google.com.ua").execute().returnContent().toString()); 
 		   System.out.println(smth); 
+		 //Проверяю строку (которая по факту  response body) на вхождение <div id="hplogo">
 		   boolean res = smth.contains("<div id=\"hplogo\">");
+		 //Вывожу результат
 		    System.out.println(res);
-		
+		    boolean expectedResult = false;
+			Assert.assertEquals(res, expectedResult);
 		
 		
 	}
